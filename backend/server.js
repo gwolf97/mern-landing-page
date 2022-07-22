bec5fb40507d471d7114fd7b97ba2a8d9fcf5576
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-07-22 18:24:22 -0500

    Mongodb connect first draft

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 import asyncHandler from "express-async-handler"
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -15,4 +20,4 @@ app.post("/signup", asyncHandler((req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
